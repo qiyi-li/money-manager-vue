4ab0eeab719cd972d4aae336c6cf5000b72729cd
errgou <qiyi.li@aol.com>
2020-05-13 21:54:29 +0800

    初步完成对 Tags 中，标签的新增

--- src/components/money/Tags.vue
+++ src/components/money/Tags.vue
@@ -2,13 +2,13 @@
   <div class="tags">
 
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
       <li v-for="tag in dataSource" :key="tag"
           :class="{selected: selectedTags.indexOf(tag)>=0}"
-          @click="toggle(tag)"
-      >{{tag}}
+          @click="toggle(tag)">
+        {{tag}}
       </li>
     </ul>
   </div>
@@ -20,7 +20,7 @@
 
   @Component
   export default class Tags extends Vue {
-    @Prop() dataSource: string[] | undefined;
+    @Prop(Array) dataSource: string[] | undefined;
     selectedTags: string[] = [];
 
     toggle(tag: string) {
@@ -30,6 +30,17 @@
       } else {
         this.selectedTags.push(tag);
       }
+    }
+
+    create() {
+      const name = window.prompt('请输入标签名');
+      if (name === '') {
+        window.alert('标签名不能为空');
+      } else if (this.dataSource) {
+        this.$emit('update:dataSource',
+            [...this.dataSource, name]);
+      }
+
     }
   }
 </script>
