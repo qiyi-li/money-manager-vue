0fc4601c8530929d04fbb916df1e8782deb0177c
errgou <qiyi.li@aol.com>
2020-05-15 22:30:42 +0800

    新增 EditLabel 组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -7,10 +7,22 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
+  import tagListModel from '@/models/tagListModel';
 
   @Component
   export default class EditLabel extends Vue {
+    created(){
+      const id=this.$route.params.id
+      tagListModel.fetch()
+      const tags=tagListModel.data
+      const tag = tags.filter(t => t.id===id)[0]
+      if(tag){
+        console.log(tag);
+      }else{
+        this.$router.replace('/404') //使用 replace ，以防止跳转 404 不能回退
+      }
 
+    }
   }
 </script>
 
