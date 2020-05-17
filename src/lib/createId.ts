b5a61cde78ad7a58ca21ec13458980f9d68c7aa9
errgou <qiyi.li@aol.com>
2020-05-17 15:29:57 +0800

    修复 ID 修复 ID 不变的 bug

--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -2,7 +2,7 @@ let id: number = parseInt(window.localStorage.getItem('_id_max') || '0') || 0;
 
 function createId() {
   id++;
-  window.localStorage.setItem('_idMax', id.toString());
+  window.localStorage.setItem('_id_max', id.toString());
   return id;
 }
 
