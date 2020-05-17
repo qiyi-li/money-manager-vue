71b606c7b351ef7e4ee95dc60125ff8b7d69ae57
errgou <qiyi.li@aol.com>
2020-05-17 10:09:11 +0800

    添加 id 至 localStorage

--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -1,7 +1,9 @@
-let id: number= parseInt(window.localStorage.getItem('_id_max') || '0') || 0
-function createId(){
-  id++
-  return id
+let id: number = parseInt(window.localStorage.getItem('_id_max') || '0') || 0;
+
+function createId() {
+  id++;
+  window.localStorage.setItem('_idMax', id.toString());
+  return id;
 }
 
-export default createId
+export default createId;
