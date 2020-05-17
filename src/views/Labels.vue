c8052ee099a8fe5456868e32fba5c3ec7ca15f87
errgou <qiyi.li@aol.com>
2020-05-17 11:45:48 +0800

    封装 window.createTag

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -33,15 +33,7 @@
             createTag() {
                   const name = window.prompt('请输入标签名');
                   if (name) {
-
-                        const message=tagListModel.create(name)
-                              if (message === 'duplicated') {
-                                    window.alert('标签名重复');
-                              } else if (message === 'success') {
-                                    window.alert('添加成功');
-                              }else  if(message==='null'){
-                                    window.alert('标签不能为空');
-                              }
+                        window.createTag(name)
                         }
                   }
 
