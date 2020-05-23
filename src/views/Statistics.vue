218f2d11e410c19f2df77feb08156905a8914540
errgou <qiyi.li@aol.com>
2020-05-23 17:48:05 +0800

    利用 dayjs 简化时间显示

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,8 +4,8 @@
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
 
     <ol>
-      <li v-for="(group,index) in result" :key="index">
-        <h3 class="title">{{group.title}}</h3>
+      <li v-for="group in result" :key="group.title">
+        <h3 class="title">{{beautify(group.title)}}</h3>
         <ol>
           <li v-for="item in group.items" :key="item.id" class="record">
             <span>{{tagString(item.tags)}}</span>
@@ -18,43 +18,36 @@
 
   </Layout>
 </template>
-<style scoped lang="scss">
-  %item {
-    padding: 8px 16px;
-    line-height: 24px;
-    display: flex;
-    justify-content: space-between;
-    align-content: center;
-  }
-
-  .title {
-    background-color: #d9d9d9;
-    @extend %item
-  }
-
-  .record {
-    background-color: #fff;
-    @extend %item
-  }
-  .notes{
-    margin-right: auto;
-    margin-left: 16px;
-    color:#999;
-  }
-</style>
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import Tabs from '@/components/Tabs.vue';
   import intervalList from '@/constants/intervalList';
   import recordTypeList from '@/constants/recordTypeList';
+  import dayjs from 'dayjs';
 
   @Component({
     components: {Tabs},
   })
   export default class Statistics extends Vue {
     tagString(tags: Tag[]) {
-      return tags.length === 0 ? '无' :   tags.join(',');
+      return tags.length === 0 ? '无' : tags.join(',');
+    }
+
+    beautify(string: string) {
+      const now = dayjs();
+      const day = dayjs(string);
+      if (day.isSame(now, 'day')) {
+        return '今天';
+      } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+        return '昨天';
+      } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+        return '前天';
+      } else if (day.isSame(now, 'year')) {
+        return day.format('M月D日');
+      } else {
+        return day.format('YYYY年M月D日');
+      }
     }
 
     get recordList() {
@@ -103,5 +96,29 @@
     .interval-tabs-item {
       height: 48px;
     }
+  }
+
+  %item {
+    padding: 8px 16px;
+    line-height: 24px;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+  }
+
+  .title {
+    background-color: #d9d9d9;
+    @extend %item
+  }
+
+  .record {
+    background-color: #fff;
+    @extend %item
+  }
+
+  .notes {
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999;
   }
 </style>
