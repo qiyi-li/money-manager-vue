bd022f2c3a9eead744373f39b40a168b0c195d29
errgou <qiyi.li@aol.com>
2020-05-24 11:08:59 +0800

    使用非手机打开时，添加提示

--- src/main.ts
+++ src/main.ts
@@ -21,8 +21,19 @@ new Vue({
   render: h => h(App)
 }).$mount('#app');
 
-window.onload=function(){
-  setTimeout(function() {
-    window.scrollTo(0, 200)
-  }, 0)
+window.onload = function () {
+  setTimeout(function () {
+    window.scrollTo(0, 200);
+  }, 0);
+};
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('为保证浏览效果，请用手机打开此页面');
+  const img = document.createElement('img',);
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.right = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow='0 0 10px rgba(0,0,0,0.25)'
+  document.body.appendChild(img);
 }
