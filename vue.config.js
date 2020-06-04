1e3b8df83d67794fa0ca89d9f58d8bd96c764abd
liubai <errgou@gmail.com>
2020-06-04 21:37:15 +0800

    修改title

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/liubaijizhangwebsite/'
+    ? '/money-manager-webseite/'
     : '/',
   lintOnSave: false,
   chainWebpack: config => {
